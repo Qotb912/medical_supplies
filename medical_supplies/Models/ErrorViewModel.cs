933a80630b9c3404fb32515e680216b0ef3aba1e
Qotb912 <mahmoudqotb912@gmail.com>
2023-10-16 13:58:13 +0300

    Add project files.

---
+++ medical_supplies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace medical_supplies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
